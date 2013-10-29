9135076f29b805b000fea31bcdabff6a1914f726
Shravan Reddy <Vayu@Shravans-MacBook-Pro.local>
2013-10-29 00:41:19 -0700

    setup.py to build xcodeproj

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+# System modules
+import sys
+import os
+import shlex
+import subprocess
+import shutil
+import platform
+
+# Library modules
+from setuptools import setup, find_packages
+
+if sys.version_info[:2] < (2, 7):
+  print "Sorry, pyglass requires python version 2.7 or later"
+  sys.exit(1)
+
+if platform.system() != 'Darwin':
+  print "Sorry, pyglass only runs on OS X"
+  sys.exit(1)
+
+def rm_tempdirs():
+  ''' Remove temporary build folders '''
+  tempdirs = ['build', 'dist', os.path.join('QuickGlass', 'build')]
+  for tempdir in tempdirs:
+    if os.path.exists(tempdir):
+      shutil.rmtree(tempdir, ignore_errors=True)
+
+def xcodebuild():
+  ''' Build & move the QuickGlass binary to lib '''
+  os.chdir('QuickGlass')
+
+  cmd = 'xcodebuild build'
+  subprocess.call(shlex.split(cmd))
+
+  os.chdir('..')
+
+  if not os.path.exists('lib'):
+    os.mkdir('lib')
+
+  if os.path.exists('lib/QuickGlass'):
+    os.remove('lib/QuickGlass')
+
+  shutil.move('QuickGlass/build/Release/QuickGlass', 'lib')
+
+rm_tempdirs()
+xcodebuild()
+rm_tempdirs() # Cleanup after ourselves
