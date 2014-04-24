0b66ff2c578736526633170902c6a897e65fbc7f
Shravan Reddy <shravan@cs.stanford.edu>
2014-04-23 17:17:42 -0700

    temporarily disable thumbnail_preview

--- pyglass/export.py
+++ pyglass/export.py
@@ -37,4 +37,25 @@ def thumbnail_preview(src_path, max_width, max_height, format):
     if subprocess.call(shlex.split(cmd)) == 0:
       return dest_path
 
+def thumbnail_preview(src_path):
+  """ Returns the path to small thumbnail preview. """
+  return None
+
+  try:
+    assert(exists(src_path))
+
+    max_width, max_height = 2640, 1520
+    export_format = 'png'
+
+    with NamedTemporaryFile(prefix='pyglass', delete=False) as tempfileobj:
+      dest_path = tempfileobj.name
+
+    cmd = u'%s -srcPath "%s" -destPath "%s" -maxWidth %f -maxHeight %f -exportFormat "%s"' % \
+          (GLASS_LIB, src_path, dest_path, max_width, max_height, export_format)
+    print cmd
+    assert(subprocess.call(shlex.split(cmd)) == 0)
+
+    assert(exists(dest_path))
+    return dest_path
+  except:
     return None
