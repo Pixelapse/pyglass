ea7768b728d5c5e2bad00f6b821462266463959f
Shravan Reddy <shravan@cs.stanford.edu>
2014-04-23 17:18:30 -0700

    extract generator preview

--- pyglass/api.py
+++ pyglass/api.py
@@ -7,6 +7,9 @@ def export_preview(src_path):
   preview_path = export.embedded_preview(src_path)
 
   if not preview_path:
+    preview_path = export.generator_preview(src_path)
+
+  if not preview_path:
     preview_path = export.thumbnail_preview(src_path)
 
   return preview_path
