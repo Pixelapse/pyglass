5962222a6b8e0637d6c614f66917bba9003a60be
Shravan Reddy <shravan@cs.stanford.edu>
2014-06-20 17:48:21 -0700

    test importing from process module

--- pyglass/sketch.py
+++ pyglass/sketch.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from process import check_call
+
 from .settings import SKETCHTOOL
 
-# def pages(src_path):
-#   try:
+def pages_preview(src_path):
+  try:
 
-#   except Exception as e:
-#     print u'Unable to extract pages: %s' % e
-#     return None
+  except Exception as e:
+    print u'Unable to extract pages: %s' % e
+    return None
