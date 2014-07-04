e89fdf9face95475a2502680fa839d7276ac5a93
Shravan Reddy <shravan@cs.stanford.edu>
2014-07-03 20:26:22 -0700

    temp comment out export_preview

--- pyglass/api.py
+++ pyglass/api.py
@@ -3,17 +3,17 @@
 from . import quicklook, sketch
 
 
-def export_preview(src_path):
-  if sketch.is_sketchfile(src_path):
-    preview_path = sketch.export_pages(src_path)
+# def export_preview(src_path):
+#   if sketch.is_sketchfile(src_path):
+#     preview_path = sketch.export_pages(src_path)
 
-  if not preview_path:
-    preview_path = quicklook.embedded_preview(src_path)
+#   if not preview_path:
+#     preview_path = quicklook.embedded_preview(src_path)
 
-  if not preview_path:
-    preview_path = quicklook.generator_preview(src_path)
+#   if not preview_path:
+#     preview_path = quicklook.generator_preview(src_path)
 
-  if not preview_path:
-    preview_path = quicklook.thumbnail_preview(src_path)
+#   if not preview_path:
+#     preview_path = quicklook.thumbnail_preview(src_path)
 
-  return preview_path
+#   return preview_path
