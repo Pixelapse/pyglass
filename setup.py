1ed4f749edd0c7cf79ceabca223e8cb1b0de8552
Shravan Reddy <shravan@cs.stanford.edu>
2014-07-08 18:27:03 -0700

    don't require pillow

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ setup(
   author_email='shravan@pixelapse.com',
   packages=find_packages(),
   package_data={'': package_libs},
-  install_requires=['process', 'pyunicode', 'Pillow'],
+  install_requires=['process', 'pyunicode'],
   include_package_data=True,
   zip_safe=False,
   license=open('LICENSE').read()
