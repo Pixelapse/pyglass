bc838034f939237bfbdf8296cad810d5126bfedd
Shravan Reddy <shravan@cs.stanford.edu>
2014-07-11 12:52:33 -0700

    graffle test

--- tests/test_preview.py
+++ tests/test_preview.py
@@ -2,7 +2,6 @@
 
 # Default libs
 import os
-import logging
 import unittest
 import time
 
@@ -14,27 +13,25 @@ import pyglass
 
 from tests.helpers import data_file
 
-logger = logging.getLogger(__name__)
-
 
 ############################################################
 # BASE CLASSES
 ############################################################
 class PreviewTestCase(unittest.TestCase):
   def setUp(self):
-    logger.info(u'******* RUNNING %s TEST *******' % self.id())
+    print u'******* RUNNING %s TEST *******' % self.id()
     self.time_start = time.time()
 
   def tearDown(self):
     self.time_elapsed = time.time() - self.time_start
-    logger.info(u'TIME TO: %s: %s secs' % (self.id(), self.time_elapsed))
+    print u'TIME TO: %s: %s secs' % (self.id(), self.time_elapsed)
 
-  def _test_preview(self, file_path, multipage=False):
+  def _test_preview(self, file_path, num_pages=1):
     preview_path = pyglass.preview(file_path)
     self.assertIsNotNone(preview_path)
 
     mimetype = magic.from_file(preview_path, mime=True).lower()
-    if multipage:
+    if num_pages > 1:
       self.assertEqual(mimetype, pyglass.models.ExportMimeType.PDF)
     else:
       self.assertEqual(mimetype, pyglass.models.ExportMimeType.PNG)
@@ -53,7 +50,15 @@ class TestSketch(PreviewTestCase):
     self._test_preview(data_file('sketch/large.sketch'))
 
   def test_pages(self):
-    self._test_preview(data_file('sketch/pages.sketch'), multipage=True)
+    self._test_preview(data_file('sketch/pages.sketch'), num_pages=3)
 
   def test_unicode(self):
-    self._test_preview(data_file('sketch/unicode.sketch'), multipage=True)
+    self._test_preview(data_file('sketch/unicode.sketch'), num_pages=2)
+
+
+############################################################
+# TEST CASES
+############################################################
+class TestGraffle(PreviewTestCase):
+  def test_pages(self):
+    self._test_preview(data_file('graffle/pages.graffle'), num_pages=2)
