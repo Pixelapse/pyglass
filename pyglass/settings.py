94952ee78a5cc73491ce25e6b1e36e7d7c23d0c0
Shravan Reddy <shravan@cs.stanford.edu>
2014-07-28 22:13:47 -0700

    make sure sketch tool is executable on the installed machine

--- pyglass/settings.py
+++ pyglass/settings.py
@@ -1,7 +1,34 @@
 # -*- coding: utf-8 -*-
+# Default libs
+import os
+import stat
+
 from os.path import dirname, join, abspath
 
 curr_dir = dirname(abspath(__file__))
-SKETCHTOOL = join(curr_dir, 'lib', 'SketchTool', 'sketchtool')
-QUICKGLASS = join(curr_dir, 'lib', 'QuickGlass')
+
+
+def make_executable(path_str):
+  ''' Performs the equivalent of `chmod +x` on the file at path_str.
+  :returns: path_str if success, else None
+  '''
+  try:
+    mode = os.stat(path_str).st_mode
+    os.chmod(path_str, mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
+  except Exception as e:
+    print 'Exception: %s' % e
+    return None
+  return path_str
+
+
+def sketchtool_executable():
+  return make_executable(join(curr_dir, 'lib', 'SketchTool', 'sketchtool'))
+
+
+def quickglass_executable():
+  return make_executable(join(curr_dir, 'lib', 'QuickGlass'))
+
+
+SKETCHTOOL = sketchtool_executable()
+QUICKGLASS = quickglass_executable()
 QLMANAGE = join('/usr', 'bin', 'qlmanage')
